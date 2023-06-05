8471f1287790616f416ea0fce46b6406d708909c
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-06-05 15:07:36 -0300

    Trabalho-De-Arquitetura-De-Software
    Foram feitos os ajustes no CSS, no <main> e no template home.mustache, bem como nas rotas no arquivo index.js.

--- aplicacaoDistribuida/routes/index.js
+++ aplicacaoDistribuida/routes/index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+//Rota Principal
 router.get('/', (req, res) => {
+  res.render('home');
+});
+
+//Rota de Login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+//Fota de da Home
+router.get('/home', (req, res) => {
   res.render('home');
 });
 

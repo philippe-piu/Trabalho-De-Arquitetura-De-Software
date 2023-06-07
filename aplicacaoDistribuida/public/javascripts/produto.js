ef5d51c763e0aaacc0054952a1cef18e0d20b2b0
Caio Vinicius <caiovbafer@gmail.com>
2023-06-07 19:58:31 -0300

    Atualizada a entidade produto.
    Retirado trecho do código referente a como usar.

--- aplicacaoDistribuida/public/javascripts/produto.js
+++ aplicacaoDistribuida/public/javascripts/produto.js
@@ -18,11 +18,3 @@ class Produto {
     return produto;
   }
   
-  // Exemplo de uso da função
-  const produto1 = armazenarProduto('Produto A', 'Descrição do Produto A', 10.99);
-  console.log(produto1);
-  
-  const produto2 = armazenarProduto('Produto B', 'Descrição do Produto B', 19.99);
-  console.log(produto2);
-  console.log(produtos);
-  

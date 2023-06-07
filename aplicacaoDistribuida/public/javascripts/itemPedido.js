2c454b801f6afc3afd4374b7d97ae143b46e0ecd
Caio Vinicius <caiovbafer@gmail.com>
2023-06-07 19:47:33 -0300

    Logica de armazenamento
    Criada a logica de armazenamento na memoria para as entidades itemPedido e produto.

--- aplicacaoDistribuida/public/javascripts/itemPedido.js
+++ aplicacaoDistribuida/public/javascripts/itemPedido.js
@@ -4,3 +4,16 @@ class ItemPedido {
       this.quantidade = quantidade;
     }
   }
+  
+  const itensPedidos = [];
+  
+  function armazenarItemPedido(produto, quantidade) {
+    const itemPedido = new ItemPedido(produto, quantidade);
+  
+    // Armazena o item de pedido no array itensPedidos
+    itensPedidos.push(itemPedido);
+  
+    // Retorna o objeto item de pedido criado
+    return itemPedido;
+  }
+  
--- aplicacaoDistribuida/public/javascripts/produto.js
+++ aplicacaoDistribuida/public/javascripts/produto.js
@@ -5,3 +5,24 @@ class Produto {
       this.preco = preco;
     }
   }
+  
+  const produtos = [];
+  
+  function armazenarProduto(nome, descricao, preco) {
+    const produto = new Produto(nome, descricao, preco);
+  
+    // Armazena o produto no array produtos
+    produtos.push(produto);
+  
+    // Retorna o objeto produto criado
+    return produto;
+  }
+  
+  // Exemplo de uso da função
+  const produto1 = armazenarProduto('Produto A', 'Descrição do Produto A', 10.99);
+  console.log(produto1);
+  
+  const produto2 = armazenarProduto('Produto B', 'Descrição do Produto B', 19.99);
+  console.log(produto2);
+  console.log(produtos);
+  

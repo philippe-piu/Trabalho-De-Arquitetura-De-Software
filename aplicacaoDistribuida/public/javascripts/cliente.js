c33feddc1e05d87e84d13c4061a0d0ee18b3b38d
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-06-07 20:37:33 -0300

    Trabalho-De-Arquitetura-De-Software
    variavel  cidade na  cliente.js

--- aplicacaoDistribuida/public/javascripts/cliente.js
+++ aplicacaoDistribuida/public/javascripts/cliente.js
@@ -3,13 +3,14 @@ class Cliente {
       this.nome = nome;
       this.endereco = endereco;
       this.telefone = telefone;
+      this.cidade = cidade;
     }
   }
   
   const clientes = [];
   
-  function armazenarCliente(nome, endereco, telefone) {
-    const cliente = new Cliente(nome, endereco, telefone);
+  function armazenarCliente(nome, endereco, telefone,cidade) {
+    const cliente = new Cliente(nome, endereco, telefone, cidade);
   
     // Armazena o cliente no array clientes
     clientes.push(cliente);

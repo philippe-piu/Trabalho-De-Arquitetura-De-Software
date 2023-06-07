5715902113f6ef8e62384470c7fe17a63a02ca63
Caio Vinicius <caiovbafer@gmail.com>
2023-06-07 19:35:07 -0300

    Função de armazenamento
    Criada a função de armazenamento da entidade cliente e pedido.

--- aplicacaoDistribuida/public/javascripts/cliente.js
+++ aplicacaoDistribuida/public/javascripts/cliente.js
@@ -5,3 +5,16 @@ class Cliente {
       this.telefone = telefone;
     }
   }
+  
+  const clientes = [];
+  
+  function armazenarCliente(nome, endereco, telefone) {
+    const cliente = new Cliente(nome, endereco, telefone);
+  
+    // Armazena o cliente no array clientes
+    clientes.push(cliente);
+  
+    // Retorna o objeto cliente criado
+    return cliente;
+  }
+  
--- aplicacaoDistribuida/public/javascripts/pedido.js
+++ aplicacaoDistribuida/public/javascripts/pedido.js
@@ -1,9 +1,20 @@
-class Pedido {
-    constructor(cliente, numero, data, status) {
+  class Pedido {
+    constructor(cliente, numero) {
       this.cliente = cliente;
       this.numero = numero;
-      this.data = data;
-      this.status = status;
       this.itens = [];
     }
   }
+  
+  const pedidos = [];
+  
+  function armazenarPedido(cliente, numero) {
+    const pedido = new Pedido(cliente, numero);
+  
+    // Armazena o pedido no array pedidos
+    pedidos.push(pedido);
+  
+    // Retorna o objeto pedido criado
+    return pedido;
+  }
+  
